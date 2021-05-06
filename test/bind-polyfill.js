9f5ba690506433a6eb96dd52d651f43cf04fb10d
hulkoba <jacoba@neighbourhood.ie>
2021-05-06 12:46:10 +0200

    feat: apply linting via standard

--- test/bind-polyfill.js
+++ test/bind-polyfill.js
@@ -1,28 +1,29 @@
 (function () {
-  'use strict';
+  'use strict'
   // minimal polyfill for phantomjs; in the future, we should do ES5_SHIM=true like pouchdb
   if (!Function.prototype.bind) {
+    // eslint-disable-next-line no-extend-native
     Function.prototype.bind = function (oThis) {
-      if (typeof this !== "function") {
+      if (typeof this !== 'function') {
         // closest thing possible to the ECMAScript 5
         // internal IsCallable function
-        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable')
       }
 
-      var aArgs = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
-          fNOP = function () {},
-          fBound = function () {
-            return fToBind.apply(this instanceof fNOP && oThis
-                   ? this
-                   : oThis,
-                   aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
+      const aArgs = Array.prototype.slice.call(arguments, 1)
+      const fToBind = this
+      const FNOP = function () {}
+      const fBound = function () {
+        return fToBind.apply(this instanceof FNOP && oThis
+          ? this
+          : oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments)))
+      }
 
-      fNOP.prototype = this.prototype;
-      fBound.prototype = new fNOP();
+      FNOP.prototype = this.prototype
+      fBound.prototype = new FNOP()
 
-      return fBound;
-    };
+      return fBound
+    }
   }
-})();
+})()
